a78c4abc5612dcef4be69ecdd91f789a684ecdbe
mwunnam <mwunnam@gmail.com>
2025-11-08 11:07:52 +0000

    correction

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,20 @@
+
+export default function Header() {
+  return (
+    <>
+      <header className="flex bg-gray">
+        <div>
+          <h2>Logo</h2>
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Blog</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
