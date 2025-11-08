51048848945d8023639eb9938d1458079f4127b9
mwunnam <mwunnam@gmail.com>
2025-11-08 23:18:32 +0000

    done with task 5

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button"
 export default function About() {
   return (
     <>
@@ -6,6 +7,7 @@ export default function About() {
       <main className="p-6">
         <h2 className="text-2xl font-semibold">About Us</h2>
         <p className="mt-2 text-gray-600">We Learning On going</p>
+        <Button label="Click Me" size="large" shape="rounded-md" />
       </main>
     </>
   );
